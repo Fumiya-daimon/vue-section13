7238d6c75dc05036bccda79965771166b39ffeba
Fumiya-daimon <72593272+Fumiya-daimon@users.noreply.github.com>
2023-02-22 22:25:51 +0900

    185

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MyHome from './views/MyHome.vue'
+import MyUsers from './views/MyUsers.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/', component: MyHome },
+    { path: '/users', component: MyUsers }
+  ]
+})
