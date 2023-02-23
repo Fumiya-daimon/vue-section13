02fc6adcb2e407d8624b2e88f7009e42518287a6
Fumiya-daimon <72593272+Fumiya-daimon@users.noreply.github.com>
2023-02-23 10:52:01 +0900

    204

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <router-view name="header"></router-view>
-    <router-view></router-view>
+      <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+</style>
