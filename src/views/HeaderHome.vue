5275bd4243db8572fc4f0fa8718fd5d37dbc8edf
Fumiya-daimon <72593272+Fumiya-daimon@users.noreply.github.com>
2023-02-24 12:47:10 +0900

    215216

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,8 +3,23 @@
     <h2>Homeです</h2>
       <router-link to="/" active-class="link--active" exact>Home</router-link>
       <router-link to="/users" active-class="link--active" exact>Users</router-link>
+      <button @click="increment">+1</button>
+      <button @click="decrement">+1</button>
     </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      return this.$store.state.count++
+    },
+    decrement() {
+      return this.$store.state.count--
+    }
+  },
+}
+</script>
 
 <style scoped>
   .link {
